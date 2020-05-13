8ab8af0ba4e52fa14afcb90a424b04b917b0d415
Smint <ssg.jss2018@gmail.com>
2020-05-13 21:54:21 +0530

    Update rng.js

--- rng.js
+++ rng.js
@@ -5,15 +5,17 @@ function getRandomUniformDistribution() {
     return Math.random();
 }
 function getRandomUniqueIdentifer() {
-    var u = '',
-        i = 0;
-    while (i++ < 36) {
-        var c = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx' [i - 1],
-            r = Math.random() * 16 | 0,
-            v = c == 'x' ? r : (r & 0x3 | 0x8);
-        u += (c == '-' || c == '4') ? c : v.toString(16);
-    }
-    return u;
+var s = [];
+var hexDigits = "0123456789abcdef";
+for (var i = 0; i < 36; i++) {
+    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+}
+s[14] = "4";
+s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);
+s[8] = s[13] = s[18] = s[23] = "-";
+
+var uuid = s.join("");
+return uuid;
 }
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
@@ -22,7 +24,7 @@ function getRandomArbitrary(min, max) {
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function getRandomBinaryInt() {
