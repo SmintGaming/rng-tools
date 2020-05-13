07ea51591583f274d0b061ff96aa6226371fc324
Smint <ssg.jss2018@gmail.com>
2020-05-13 21:02:13 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ console.log("A Random Arbitrary Value Between 1 & 100: " + rngtools.getRandomArb
 console.log("A Random Value Between 1 & 100: " + rngtools.getRandomInt(1, 100)); 
 console.log("A Random Value Between 1 & 100 (Inclusive): " + rngtools.getRandomIntInclusive(1, 100));
 console.log("A Random Poisson Distribution (Mean=4): " + rngtools.getRandomPoissonDistribution(4));
-console.log("A Random Uniform Distribution:  " + rngtools.getRandomUniformDistribution(4));
+console.log("A Random Uniform Distribution:  " + rngtools.getRandomUniformDistribution());
 console.log("A Random Binary Value: " + rngtools.getRandomBinaryInt());
 console.log("A Unique Identifier: " + rngtools.getRandomUniqueIdentifer());
