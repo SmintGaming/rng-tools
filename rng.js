e39bcf89b7072661e12fe827a845e12ea0138c9f
Smint <ssg.jss2018@gmail.com>
2020-05-13 14:42:58 +0530

    Added More Features

--- rng.js
+++ rng.js
@@ -2,7 +2,15 @@ function getRandomUniformDistribution() {
     return Math.random();
 }
 function getRandomUniqueIdentifer() {
-    return Math.random() * getRandomArbitrary(1,0) + JSON.stringify(Date.now());
+    var u = '',
+        i = 0;
+    while (i++ < 36) {
+        var c = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx' [i - 1],
+            r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        u += (c == '-' || c == '4') ? c : v.toString(16);
+    }
+    return u;
 }
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var rngtools = require("./rng.js");
-rngtools.dice();
 console.log("Some 6-Sided Die: " + rngtools.dice());
+console.log("A Random Arbitrary Value Between 1 & 100: " + rngtools.getRandomArbitrary(1, 100));
 console.log("A Random Value Between 1 & 100: " + rngtools.getRandomInt(1, 100)); 
 console.log("A Random Value Between 1 & 100 (Inclusive): " + rngtools.getRandomIntInclusive(1, 100));
 console.log("A Random Poisson Distribution (Mean=4): " + rngtools.getRandomPoissonDistribution(4));
